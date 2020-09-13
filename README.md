eb389ac53576880a4303afbb30fd98bd8e56380c
emibcn <emi@algorismia.com>
2020-09-13 20:20:52 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Covid Data
+
+This repo is used to store and serve daily collected data from https://dadescovid.org (institutional data published by the __Generalitat de Catalunya__ ) into GitHub pages. The reasons for collecting this data are:
+- The data will be used from apps which don't need more than daily updates
+- Original data requests might be blocked by CORS or other technologies
+- Original servers and data might not be efficient enough
+- When applicable, normalize data from various servers
+
+The collected data is minimally adapted before publishing it:
+- Transform JS statements into JSON objects
+- Reduce some non-visible `id`'s to reduce users and servers resource consumptions
+
+This repo might collect other data in the future, from the same server, it's [backend server](https://analisi.transparenciacatalunya.cat/) or from 3rd party servers (EU statistics servers? Data collection from other territories?).
+
+# Techie
+
+The data is collected by an ugly [BASH sccript](./bin/download-map-data.sh). This script collects the interesting parts (maps SVG source, JS code with data on it) and saves them into files. The SVG files are saved transparently. The JS files are executed with NODE to ouput the collected data as JSON.
+
+This process is executed from a [GitHub Workflow](./.github/workflows/get-maps.yml) (`cron` scheduled some minuts after official data publication at 10am CEST). Once the data is obtained, deploy it to this repo' GitHub Pages in the `gh-pages` branch.
+
+# License
+
+The license of the data is the same as the original: [Open Data Commons Attribution License](http://opendatacommons.org/licenses/by/1.0/), as stated in [the backend API page](https://analisi.transparenciacatalunya.cat/Salut/Dades-setmanals-de-COVID-19-per-comarca/jvut-jxu8).
---
+++ bin/download-map-data.sh
@@ -0,0 +1,87 @@
+#!/bin/bash -e
+
+set -o pipefail
+
+get_all_url() {
+    local MAPS=( comarca municipi )
+    local TYPES=( risc taxa_conf rt edat pcrpos taxapcr )
+
+    for MAP in "${MAPS[@]}"
+    do
+	for TYPE in "${TYPES[@]}"
+        do
+            echo "${MAP} ${TYPE} https://dadescovid.cat/mapes?drop_dmapa=${TYPE}&drop_mapa=${MAP}&drop_es_residencia=0&id_html=sap_1_35&tipus=aga&codi=47"
+        done
+    done
+}
+
+transform_js_to_json() {
+    local FILE="$1"
+    local VARIABLE="$2"
+
+    # Print collected data as JSON
+    echo "console.log( JSON.stringify( ${VARIABLE} ) );" >> "${FILE}"
+
+    # Ouput JSON data to be saved
+    node "${FILE}"
+}
+
+# - Gets URL content
+# - Extract relevant data
+# - Output to JS file
+# - Execute JS file (where extracted data is printed as plain JSON)
+# - Svae JSON data to file
+parse_url() {
+    local MAP="$1"
+    local URL="$2"
+    local DL_FILE="file.html"
+    local DAYS_FILE="days.json"
+    local TMP_FILE="file.js"
+
+    # Download page and simplify id's to reduce space
+    curl -s "${URL}" \
+	| sed -e 's/'${MAP}_'/'${MAP:0:1}'_/g' \
+	> "${DL_FILE}"
+
+    # Check if we need to create the days array
+    if [ ! -f "${DAYS_FILE}" ]
+    then
+        # Extract relevant data from JS code
+        cat "${DL_FILE}" \
+            | egrep '^\s*var (dies) =' \
+            | sed -e 's/^\s*var /const /' \
+            > "${TMP_FILE}"
+
+	transform_js_to_json "${TMP_FILE}" "dies" > "${DAYS_FILE}"
+
+        # Cleanup
+        rm "${TMP_FILE}"
+    fi
+
+    # Check if we need to create the SVG for this map
+    SVG_FILE="${MAP^}.svg"
+    if [ ! -f "${SVG_FILE}" ]
+    then
+	sed -z 's#.*\(<svg.*</svg>\).*#\1#' "${DL_FILE}" > "${SVG_FILE}"
+    fi
+
+    # Extract relevant data from JS code
+    cat "${DL_FILE}" \
+	| egrep '^\s*var (titol|valor_label|valors) =' \
+	| sed -e 's/^\s*var /const /' \
+        > "${TMP_FILE}"
+
+    # Unify collected data into a single object
+    echo 'const data = {titol, valor_label, valors};' >> "${TMP_FILE}"
+
+    transform_js_to_json "${TMP_FILE}" "data"
+
+    # Cleanup
+    rm "${DL_FILE}" "${TMP_FILE}"
+}
+
+get_all_url | while read MAP TYPE URL
+do
+    echo "- Generating map for ${MAP} / ${TYPE}..."
+    parse_url "${MAP}" "${URL}" > "MapData-${MAP^}-${TYPE}.json"
+done
