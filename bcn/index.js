9b2bc3b98c2a09db59c27e0dda5327a08e100e9a
emibcn <emi@algorismia.com>
2020-10-16 17:39:37 +0200

    BCN: Allow to disable cache reading from command line.

--- bcn/index.js
+++ bcn/index.js
@@ -1,15 +1,16 @@
 import GetAllChartData from "./src/index.js";
 
 const cmdArgs = process.argv.slice(2);
-const baseHTMLFile = cmdArgs.length ? cmdArgs[0] : 'cache';
-const baseJSONFile = cmdArgs.length ? cmdArgs[1] : 'dest';
+const baseHTMLFile = cmdArgs.length     ? cmdArgs[0] : 'cache';
+const baseJSONFile = cmdArgs.length > 1 ? cmdArgs[1] : 'dest';
+const disableCache = cmdArgs.length > 2 ? cmdArgs[2] : false;
 
 console.log(process.env.NODE_ENV);
 
 try {
 
   console.log("::group::Downloading log");
-  const getter = new GetAllChartData({baseHTMLFile, baseJSONFile});
+  const getter = new GetAllChartData({baseHTMLFile, baseJSONFile, disableCache});
   const { result, errors, counters }  = await getter.get();
   console.log("::endgroup::");
 
